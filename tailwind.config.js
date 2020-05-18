8b3fdd0b60c618b25cfa5cdbad33c8dc8ee9faf0
kenigbolo <expensivestephen@hotmail.com>
2020-05-18 18:22:10 +0300

    🎨 initial page skeleton for tech solute website

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./pages/index.vue']
+  },
+  theme: {},
+  variants: {},
+  plugins: []
+}
